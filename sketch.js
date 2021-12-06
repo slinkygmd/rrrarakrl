1ae6f8432fd8a96aeca590ee4ca47592e4e81192
slinkygmd <tututinho75@gmail.com>
2021-12-06 19:45:30 -0300

    jajaja

--- sketch.js
+++ sketch.js
@@ -6,6 +6,7 @@ var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
 
+
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,6 +37,8 @@ function setup(){
 
     bird = new Bird(100,100);
 
+
+
 }
 
 function draw(){
@@ -61,4 +64,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    //adsfjouhJKHDSFIJUHKOKJHIDSAFKHIJDASFIHJUKFDSIOUJKHIKUHJFDSISDFAIHKUFDSIKKI
+
 }
